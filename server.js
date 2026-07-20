85144c1560f593d194ed5bfe65882462ce4c53c5
FOSSCharlie <github@charlie.cx>
2026-07-20 14:45:02 +0100

    Footer update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { version } = require('./package.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,10 @@ const DATA_FILE = process.env.DATA_FILE || '/data/tasks.json';
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/version', (req, res) => {
+    res.json({ version });
+});
 
 function ensureDataFile() {
     const dataDir = path.dirname(DATA_FILE);
